6a73979be3626fe41eb1047334597f420929995b
Gcampes <gabrieldecampes@gmail.com>
2015-09-23 00:50:07 -0300

    changed config files, now user parameters are saved

--- main.js
+++ main.js
@@ -2,6 +2,56 @@
 
 var app = require('app');  // Module to control application life.
 var BrowserWindow = require('browser-window');  // Module to create native browser window.
+var ipc = require('ipc'); // Communication module.
+var fs = require('fs');
+var fileDatabase = app.getPath('appData')+'/Automacao/config-database.json';
+var fileFolder = app.getPath('appData')+'/Automacao/config-folder.json';
+
+var createConfigFile = function(){
+  contentsFolder = '\
+    {\n\
+      "source": "\\\\\\\\srvCentrisTeste\\\\www\\\\ERP\\\\CentrisERP",\n\
+      "target": "C:\\\\inetpub\\\\wwwroot\\\\CentrisAutoDesenv",\n\
+      "backup": "C:\\\\inetpub\\\\wwwroot\\\\Centris Backup"\n\
+    }\n';
+  contentsDatabase = '\
+  {\n\
+    "instance": "localhost\\\\sql2012",\n\
+    "database": "DatabaseName",\n\
+    "user": "login",\n\
+    "password" : "password",\n\
+    "scripts" : "C:\\\\Projetos\\\\Source\\\\dbms\\\\SQLServer"\n\
+  }'
+
+  fs.exists(fileDatabase, function(exists){
+    if(exists)
+      console.log('existe');
+    else{
+      console.log('não existe');
+      fs.writeFile(fileDatabase, contentsDatabase, function(err) {
+          if(err) {
+              return console.log(err);
+          }
+          console.log("The file was saved!");
+      });
+    }
+  })
+
+  fs.exists(fileFolder, function(exists){
+    if(exists)
+      console.log('existe');
+    else{
+      console.log('não existe');
+      fs.writeFile(fileFolder, contentsFolder, function(err) {
+          if(err) {
+              return console.log(err);
+          }
+          console.log("The file was saved!");
+      });
+    }
+  })
+}
+
 
 // Report crashes to our server.
 require('crash-reporter').start();
@@ -25,16 +75,49 @@ app.on('ready', function() {
   // Create the browser window.
 
   //deploy
-  mainWindow = new BrowserWindow({width: 630, height: 210, resizable: false, frame: false});
+  mainWindow = new BrowserWindow({width: 630, height: 210, resizable: false, frame: false, transparent: true});
 
   //debug
-  //mainWindow = new BrowserWindow({width: 900, height: 700, resizable: false, frame: false});
+  // mainWindow = new BrowserWindow({width: 900, height: 700, resizable: false, frame: false});
 
   // and load the index.html of the app.
   mainWindow.loadUrl('file://' + __dirname + '/html/index.html');
 
   // Open the devtools.
-  //mainWindow.openDevTools();
+  // mainWindow.openDevTools();
+  createConfigFile();
+  ipc.on('asynchronous-message', function(event, arg) {
+    console.log(arg);
+    if(arg == 'configfile-database'){
+      fs.readFile(fileDatabase, 'utf8', function (err, data) {
+        if (err)
+          console.log(err);
+        else{
+          event.sender.send('asynchronous-reply', data);
+          console.log('123');
+        }
+      });
+    }
+
+    else if(arg == 'configfile-folder'){
+      fs.readFile(fileFolder, 'utf8', function (err, data) {
+        if (err)
+          console.log(err);
+        else{
+          event.sender.send('asynchronous-reply', data);
+          console.log('123');
+        }
+      });
+    }
+
+    else if(arg == 'path'){
+      event.sender.send('asynchronous-reply', app.getPath('appData'));
+    }
+
+    else{
+      mainWindow.loadUrl('file://' + __dirname + '/html/'+arg+'.html');
+    }
+  });
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function() {
